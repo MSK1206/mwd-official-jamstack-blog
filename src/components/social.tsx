4d9c506d510d4735a6a5a3e4b0975c372e600b91
MSK1206 <phantom7216@yahoo.co.jp>
2022-09-24 01:37:06 +0900

    sns components add , About page fixed .

---
+++ src/components/social.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+
+const Social: NextPage = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <a
+        href="https://github.com/MSK1206/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="bi-github github-icon-size"></i>
+      </a>
+      <a
+        href="https://github.com/MSK1206/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="bi-facebook github-icon-size"></i>
+      </a>
+      <a
+        href="https://github.com/MSK1206/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="bi-twitter github-icon-size"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import SeoHead from "@/components/seohead";
 import Image from "next/image";
 import styles from "../styles/About.module.css";
+import Social from "@/components/social";
 
 const About: NextPage = () => {
   return (
@@ -24,22 +25,51 @@ const About: NextPage = () => {
               alt="My Image"
             />
           </div>
-          <div className={styles.centerall}>
-            <h1 className={styles.myname}>Masaki Matsushita.</h1>
-            <p>birth: 1989/12/06</p>
-            <p className={styles.txtwrap}>
-              Thank you for watching our site. I am a freelance web designer in
-              Osaka.
-            </p>
-            <a
-              href="https://github.com/MSK1206/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="bi-github github-icon-size"></i>
-            </a>
+        </div>
+        <div className={styles.wrapper}>
+          <h1 className={styles.myname}>Masaki Matsushita.</h1>
+          <p>birth: 1989/12/06</p>
+          <p className={styles.txtwrap}>Thank you for watching our site.</p>
+
+          <h4 className={styles.skillsheet}>Skill Sheet</h4>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>HTML</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
+          </div>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>CSS</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
+          </div>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>javascript</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
+          </div>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>React/Next</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
+          </div>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>Vue/Nuxt</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
+          </div>
+          <div className={styles.skillbar}>
+            <h6 className={styles.skillnamebar}>Python</h6>
+            <progress id="Progress_Html" max="100" value="70">
+              70%
+            </progress>
           </div>
         </div>
+        <Social />
       </main>
     </div>
   );
